e4d7c129bfb523a95fdd4fade3d4aa843d74b1df
Shannice Hughes <kayashann@aol.com>
2024-01-19 19:00:52 -0500

    more draggable html with color

--- js/main.js
+++ js/main.js
@@ -1,119 +1,29 @@
+$(".draggable").draggable();
+
+function addItem(draggableId) {
+    var inputField = $("#" + draggableId + " input");
+    var actualItem = inputField.val();
+
+    if (actualItem.length !== 0) {
+        var createAnHTMLList = `<li>${actualItem} <button onclick="removeItem(this, '${draggableId}')">Remove</button></li>`;
+        var itemList = $("#" + draggableId + " ul");
+        itemList.append(createAnHTMLList);
+        inputField.val('');
+    }
+}
+
+function removeItem(button, draggableId) {
+    $(button).closest("li").remove();
+}
 var givenName = document.querySelector('#name')
 var btnClass = document.querySelector('#addItem')
-var listOfItems = document.querySelector('#listOfItems','#listOfItems2')
-var givenName2 = document.querySelector('#name2')
-var btnClass = document.querySelector('#addItem2')
-btnClass.addEventListener('click', () => {
-    var actualName = givenName.value
-    if (actualName.length != 0) {
-        var createAnHTMLList = `<li class=""><div>${actualName}</div><button onclick="removeNameFromTheList(this)">Remove Name</button>`
-        listOfItems.innerHTML += createAnHTMLList
-        givenName.value = ''
-        givenName.classList.remove('red')
-    } else if{
-      givenName.classList.add('red')
-    }
-    else if (actualName.length != 0) {
-      var createAnHTMLList2 = `<li class=""><div>${actualName}</div><button onclick="removeNameFromTheList(this)">Remove Name</button>`
-      listOfItems.innerHTML += createAnHTMLList2
-      givenName2.value = ''
-      givenName2.classList2.remove('red')
-    }
-    else if
-    {
-    givenName2.classList2.add('red')
-  }
-})
-
-
-function removeNameFromTheList(e) {
-    e.parentElement.remove()
-}
+var listOfItems = document.querySelector('#listOfItems')
 
 dragElement(document.getElementById("mydiv"));
-   function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
 dragElement(document.getElementById("mydiv2"));
-   function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
 dragElement(document.getElementById("mydiv3"));
+dragElement(document.getElementById("mydiv4"));
+
    function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
@@ -154,45 +64,4 @@ dragElement(document.getElementById("mydiv3"));
     document.onmousemove = null;
   }
 }
-dragElement(document.getElementById("mydiv4"));
-   function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-} 
 
